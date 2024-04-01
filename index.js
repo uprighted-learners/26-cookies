23a220e322c679c8dbcf6b8ba07d7f125936c25d
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-04-01 17:58:15 -0500

    cookies example

---
+++ index.js
@@ -0,0 +1,39 @@
+// import dependencies
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const PORT = 8080;
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/api/health', (req, res) => {
+    res.send('Hello World!');
+});
+
+// cookie parse endpoint
+app.get('/', (req, res) => {
+    // cookies that have not been signed
+    console.log("Unsigned Cookies: ", req.cookies);
+
+    // cookies that have been signed
+    console.log("Signed Cookies: ", req.signedCookies);
+});
+
+// set the cart cookies
+app.get('/add/cart', (req, res) => {
+    res.cookie('cart', { items: ["bread", "milk", "cheese"] });
+    res.send('Cart cookies set!');
+});
+
+// remove the cart cookies
+app.get('/remove/cart', (req, res) => {
+    res.clearCookie('cart');
+    res.send('Cart cookies cleared!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
